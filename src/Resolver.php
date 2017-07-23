8a9e9345144c3dec847adbda0bbd31010c39045e
Zcasper <testuser@mail.linpax.org>
2017-07-23 22:53:35 +0700

    resolver interface

---
+++ src/Resolver.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link https://github.com/linpax/microphp-base
+ * @copyright Copyright &copy; 2017 Linpax
+ * @license https://github.com/linpax/microphp-base/blob/master/LICENSE
+ */
+
+namespace Micro\Base;
+
+
+interface Resolver
+{
+    public function getApp();
+
+    public function getAction();
+}
